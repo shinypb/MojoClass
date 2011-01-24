802a5b13bd70d99cd17216752665269d981a058e
Mark Christian <markc@twitter.com>
2011-01-23 18:18:00 -0800

    Tidied up a bit

--- mojo.js
+++ mojo.js
@@ -1,45 +1,44 @@
 var mojoClass = (function() {
-  var bind = function(that, obj) {
-    if(obj.bind) {
-      //  Use built-in implementation of bind
-      return obj.bind(that);
+  var mc = {
+    argumentTypes: function(args) {
+      var types = [], type;
+      var i;
+      for(i in args) {
+        type = args[i]._hasMojo ? 'mojo' : typeof(args[i]);
+        types.push(type);
+      }
+      return types.join(',');
+    },
+    bind: function(that, obj) {
+      if(obj.bind) {
+        //  Use built-in implementation of bind
+        return obj.bind(that);
+      }
+
+      /**
+       *  bind is a recent addition to ECMA-262, 5th edition; this implementation for browsers
+       *  that don't support it was found at
+       *  https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+       */
+        var slice = [].slice,
+            args = slice.call(arguments, 1),
+            self = this,
+            nop = function () {},
+            bound = function () {
+              return self.apply( this instanceof nop ? this : ( obj || {} ),
+                                  args.concat( slice.call(arguments) ) );
+            };
+
+      nop.prototype = self.prototype;
+      bound.prototype = new nop();
+
+      return bound;
     }
-
-    /**
-     *  bind is a recent addition to ECMA-262, 5th edition; this implementation for browsers
-     *  that don't support it was found at
-     *  https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
-     */
-      var slice = [].slice,
-          args = slice.call(arguments, 1),
-          self = this,
-          nop = function () {},
-          bound = function () {
-            return self.apply( this instanceof nop ? this : ( obj || {} ),
-                                args.concat( slice.call(arguments) ) );
-          };
-
-    nop.prototype = self.prototype;
-    bound.prototype = new nop();
-
-    return bound;
-  };
-
-  var argumentTypes = function(args) {
-    var types = [], type;
-    var i;
-    for(i in args) {
-      type = args[i]._hasMojo ? 'mojo' : typeof(args[i]);
-      types.push(type);
-    }
-    return types.join(',');
   };
 
   return function (/* [baseClass,] constructor, attributes */) {
     var baseClass, constructor, attributes;
-    console.log(argumentTypes(arguments));
-
-    switch(argumentTypes(arguments)) {
+    switch(mc.argumentTypes(arguments)) {
       case 'function,object':
         attributes = arguments[1];
         baseClass = null;
@@ -53,70 +52,74 @@ var mojoClass = (function() {
       case 'mojo,object':
         attributes = arguments[1];
         baseClass = arguments[0];
-        constructor = baseClass.constructor;
+        constructor = baseClass._constructor;
         break;
       case 'object':
         attributes = arguments[0];
         baseClass = null;
         constructor = function() { };
+        break;
       default:
-        throw 'Invalid mojo class declaration: ' + argumentTypes(arguments);
+        throw 'Invalid mojo class declaration: [' + mc.argumentTypes(arguments) + ']';
+    }
+
+    var extend = function(baseObject, additions) {
+      var key;
+      for(key in additions) {
+        if(additions.hasOwnProperty(key)) {
+          baseObject[key] = additions[key];
+        }
+      }
     }
 
     var mojoClassFactory = function() {
       var key;
+
+      //  Build the list of attributes this class will have. It includes everything in
+      //  the base class, plus the attributes in the class declaration.
       var classAttributes = {};
       if(baseClass) {
-        for(key in baseClass.attributes) {
-          if(!baseClass.attributes.hasOwnProperty(key)) {
-            break;
-          }
-          classAttributes[key] = baseClass.attributes[key];
-        }
+        extend(classAttributes, baseClass._attributes);
       }
-      for(key in attributes) {
-        if(!attributes.hasOwnProperty(key)) {
-          break;
-        }
-        classAttributes[key] = attributes[key];
-      }
+      extend(classAttributes, attributes);
 
+      //  Copy those attributes to this new object, binding functions as we go
       for(key in classAttributes) {
         if(!classAttributes.hasOwnProperty(key)) {
           break;
         }
         if(typeof(classAttributes[key]) === 'function') {
-          this[key] = bind(this, classAttributes[key]);
+          this[key] = mc.bind(this, classAttributes[key]);
         } else {
           this[key] = classAttributes[key];
         }
       }
 
-      //  Hook up magic
-      this._constructor = constructor;
+      //  Our _super property gives us access to the properties and methods of
+      //  the parent class; copy them over, binding the functions to this
       this._super = {};
       if(baseClass) {
-        for(key in baseClass.attributes) {
-          if(!baseClass.attributes.hasOwnProperty(key)) {
+        for(key in baseClass._attributes) {
+          if(!baseClass._attributes.hasOwnProperty(key)) {
             break;
           }
-          console.log('Super has', key, baseClass.attributes[key]);
-          if(typeof(baseClass.attributes[key]) === 'function') {
-            this._super[key] = bind(this, baseClass.attributes[key]);
+          if(typeof(baseClass._attributes[key]) === 'function') {
+            this._super[key] = mc.bind(this, baseClass._attributes[key]);
           } else {
-            this._super[key] = baseClass.attributes[key];
+            this._super[key] = baseClass._attributes[key];
           }
         }
       }
 
       //  Run the constructor
-      constructor.apply(this, arguments);
+      this._constructor = constructor;
+      this._constructor.apply(this, arguments);
 
       return this;
     };
 
     mojoClassFactory._constructor = constructor;
-    mojoClassFactory.attributes = attributes;
+    mojoClassFactory._attributes = attributes;
     mojoClassFactory._hasMojo = true;
     return mojoClassFactory;
   };
